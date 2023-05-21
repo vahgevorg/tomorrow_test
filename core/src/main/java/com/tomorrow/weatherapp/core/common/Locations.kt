f75076a98934214ad148d10aec1aeb16646d8934
Vahagn <vahagn.gevorgyan@mooncascade.com>
2023-05-21 13:52:41 +0300

    - Locations object with hardcoded coordinates

---
+++ core/src/main/java/com/tomorrow/weatherapp/core/common/Locations.kt
@@ -0,0 +1,19 @@
+package com.tomorrow.weatherapp.core.common
+
+import com.tomorrow.weatherapp.domain.model.LocationDomainModel
+
+object Locations {
+
+    val locations = listOf(
+        LocationDomainModel(53.619653, 10.079969),
+        LocationDomainModel(53.080917, 8.847533),
+        LocationDomainModel(52.378385, 9.794862),
+        LocationDomainModel(52.496385, 13.444041),
+        LocationDomainModel(53.866865, 10.739542),
+        LocationDomainModel(54.304540, 10.152741),
+        LocationDomainModel(54.797277, 9.491039),
+        LocationDomainModel(52.426412, 10.821392),
+        LocationDomainModel(53.542788, 8.613462),
+        LocationDomainModel(53.141598, 8.242565)
+    )
+}
---
+++ domain/src/main/java/com/tomorrow/weatherapp/domain/model/LocationDomainModel.kt
@@ -0,0 +1,6 @@
+package com.tomorrow.weatherapp.domain.model
+
+data class LocationDomainModel(
+    val latitude: Double,
+    val longitude: Double,
+)
