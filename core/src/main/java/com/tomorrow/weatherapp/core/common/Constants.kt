62d5d9b9d3806cdc0b46fe2f0b4909b54e8d9dd6
Vahagn <vahagn.gevorgyan@mooncascade.com>
2023-05-21 12:56:26 +0300

    - retrofit and okhtttp libraries with Koin DI setup

--- core/src/main/java/com/tomorrow/weatherapp/core/common/Constants.kt
+++ core/src/main/java/com/tomorrow/weatherapp/core/common/Constants.kt
@@ -4,4 +4,7 @@ object Constants {
 
     // presentation
     const val DEFAULT_SPLASH_TIMEOUT = 1000L
+
+    // data
+    const val API_HOST_URL = "https://api.open-meteo.com"
 }
--- data/src/main/java/com/tomorrow/weatherapp/data/di/NetModule.kt
+++ data/src/main/java/com/tomorrow/weatherapp/data/di/NetModule.kt
@@ -1,7 +1,36 @@
 package com.tomorrow.weatherapp.data.di
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.tomorrow.weatherapp.core.common.Constants.API_HOST_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val NET_MODULE = module {
-    // NO-OP
+
+    // gson
+    single<Gson> { GsonBuilder().setLenient().serializeNulls().create() }
+    single<GsonConverterFactory> { GsonConverterFactory.create(get<Gson>()) }
+
+    // interceptors
+    single { HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY) }
+
+    // okhttp clients
+    single<OkHttpClient> {
+        OkHttpClient.Builder()
+            .addInterceptor(get<HttpLoggingInterceptor>())
+            .build()
+    }
+
+    // retrofit clients
+    single<Retrofit> {
+        Retrofit.Builder()
+            .baseUrl(API_HOST_URL)
+            .addConverterFactory(get<GsonConverterFactory>())
+            .client(get())
+            .build()
+    }
 }
